3c05c8a678d9d7a98b23bcc3802d77a527c2b9cb
Tomas Gavenciak <gavento@gmail.com>
2020-03-29 20:39:51 +0200

    Fixed up reading JH data

--- datalib/import_csse.py
+++ datalib/import_csse.py
@@ -8,29 +8,66 @@ from .region_data import RegionDataset
 log = logging.getLogger(__name__)
 
 
+SKIP_NAMES = {
+    "Diamond Princess",
+    "Grand Princess",
+    "MS Zaandam",
+    "Recovered",
+}
+
+SUBSTITUTES = {
+    "Taiwan*": "Taiwan",
+    "US": "United States",
+}
+
+SUBDIVIDED_CODES = {"CA", "US", "CN", "AU"}
+
+
 def import_csse_covid(rds: RegionDataset, dir="data", prefix="JH", group="JH"):
+    skipped = set()
     for n in ["Recovered", "Confirmed", "Deaths"]:
-        d = pd.read_csv(f"{dir}/time_series_covid19_{n.lower()}_global.csv")
+        d = pd.read_csv(
+            f"{dir}/time_series_covid19_{n.lower()}_global.csv",
+            dtype={"Country/Region": "string", "Province/State": "string"},
+        )
         codes = []
+
+        def skip(r):
+            skipped.add(f"{r['Country/Region']}/{r['Province/State']}")
+            codes.append("")
+
         for k, r in d.iterrows():
             prov = r["Province/State"]
+            prov = SUBSTITUTES.get(prov, prov)
             country = r["Country/Region"]
+            country = SUBSTITUTES.get(country, country)
+            if prov in SKIP_NAMES or country in SKIP_NAMES:
+                skip(r)
+                continue
             cs = rds.find_all(country, levels="country")
             if len(cs) != 1:
-                log.warning(f"Non-unique country match for {country}: {cs}")
-                codes.append("")
+                log.debug(f"Non-unique country match for {country}: {cs}")
+                skip(r)
                 continue
             c = cs[0]
+
             if pd.isna(prov):
+                # Add country
                 codes.append(c)
             else:
-                ps = rds.find_all(country, levels="subdivision")
+                # Add province
+                if c not in SUBDIVIDED_CODES:
+                    skip(r)
+                    continue
+                ps = rds.find_all(prov, levels="subdivision")
                 ps = [p for p in ps if rds[p].Country == c]
                 if len(ps) != 1:
-                    log.warning(f"Non-unique subdivision match for {country}/{prov}: {ps}")
-                    codes.append("")
+                    log.debug(f"Non-unique subdivision match for {country}/{prov}: {ps}")
+                    skip(r)
                     continue
+                print(ps)
                 codes.append(ps[0])
+
         d["Code"] = codes
         d = d[d.Code != ""]
         destname = f"{prefix}_{n}"
@@ -49,3 +86,5 @@ def import_csse_covid(rds: RegionDataset, dir="data", prefix="JH", group="JH"):
                 - rds.series[(f"{prefix}_Deaths", date)]
             )
             rds.add_column(val, group, date=date, name=f"{prefix}_Active")
+
+    log.info(f"Skipped {len(skipped)} records: {skipped!r}")
