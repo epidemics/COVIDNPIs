18f478b40de45d8a3c34a5b1b154c4fe4aa664da
Tomas Gavenciak <gavento@gmail.com>
2020-04-02 01:58:29 +0200

    Reformat and update action config

--- tests/test_regions.py
+++ tests/test_regions.py
@@ -1,8 +1,9 @@
 import epimodel
 
+
 def test_load_index_region(datadir):
     rds = epimodel.RegionDataset.load(datadir / "regions.csv")
-    assert rds['CZ'].CountryCodeISOa3 == "CZE"
+    assert rds["CZ"].CountryCodeISOa3 == "CZE"
     r = rds.find_one_by_name("Czech Republic")
     assert r.Code == "CZ"
     assert r.CountryCode == "CZ"
