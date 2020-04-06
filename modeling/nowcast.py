2517d893537129b2a59683a6836bb92b035c17e2
sualehasif996 <sualehasif996@gmail.com>
2020-04-06 12:56:39 -0400

    Added some basic nowcasting code

---
+++ modeling/nowcast.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+from epimodel import RegionDataset, read_csv
+from modeling.nowcasting.nowcasting import exp_model
+
+rds = RegionDataset.load('../data/regions.csv')
+csse_ds = read_csv('../data/CSSE.csv')
+
+
+def exp_csse(data, actual_pred=None, date=None):
+    case_to_pred = "Confirmed"
+    days_prev = 7
+    days_to_pred = 7
+
+    # Getting all the regions
+    regions = data.index.get_level_values(0).unique().to_list()
+    data_x = np.linspace(1, days_prev, days_prev)
+
+    for region in regions:
+        reg_data = data.loc[region]
+        reg_data_prev = reg_data[-1 * days_prev:]
+        data_y = reg_data_prev[case_to_pred].to_list()
+
+        if actual_pred is None:
+            actual_pred = data_y[-1]
+        if date is None:
+            date = reg_data_prev.index[-1]
+
+        print("-----------------")
+        print(region)
+        print(data_x, data_y, days_prev, days_to_pred, actual_pred)
+
+        pred_next_week = None
+
+        try:
+            pred_next_week = exp_model(data_x, data_y, date, days_prev, days_to_pred, actual_pred)
+        except ValueError:
+            print(region, "data has NaNs")
+        except Exception as inst:
+            print("There was an error doing the forecasting")
+            print(type(inst), inst)
+
+        print(region, pred_next_week)
---
+++ modeling/nowcasting/nowcasting.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+from epimodel import RegionDataset, read_csv
+
+
+def exp_model(data_x, data_y, date, days_prev, days_to_pred, actual_pred=None):
+    days_prev = int(days_prev)
+    days_to_pred = int(days_to_pred)
+
+    # Define exponential fit function
+    def func(x, a, b, c):
+        return a * np.exp(-b * x) + c
+
+    if actual_pred is None:
+        actual_pred = data_y[-1]
+
+    popt, pcov = curve_fit(func, data_x, data_y, maxfev=2000)
+
+    datelist = pd.date_range(start=date, periods=days_to_pred).tolist()
+
+    data_xp = np.linspace(days_prev + 1, days_prev + days_to_pred, days_to_pred)
+
+    pred_next_week = [[datelist[i], int(func(data_xp[i], *popt))] for i in range(days_to_pred)]
+
+    return pred_next_week
