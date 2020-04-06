751fdc097af40336b9bf4977c2c5c43b0895b015
eohomegrownapps <euan.l.y.ong@gmail.com>
2020-04-06 22:24:55 +0100

    Black formatting
    Ran this through a Black formatter

--- epimodel/imports/johns_hopkins.py
+++ epimodel/imports/johns_hopkins.py
@@ -27,19 +27,28 @@ SUBSTITUTE_PROVINCE = {}
 
 SUBDIVIDED_COUNTRIES = {"CA", "US", "CN", "AU"}
 
-SUBDIVIDED_DATASETS = ["US"] # datasets with more granular data
+SUBDIVIDED_DATASETS = ["US"]  # datasets with more granular data
 
 DROP_COLUMNS = {
-    "US": ["UID", "iso2", "iso3", "code3", "FIPS", "Admin2", "Combined_Key", "Population", "Lat", "Long", "Long_"],
-    "global": ["Lat","Long"]
+    "US": [
+        "UID",
+        "iso2",
+        "iso3",
+        "code3",
+        "FIPS",
+        "Admin2",
+        "Combined_Key",
+        "Population",
+        "Lat",
+        "Long",
+        "Long_",
+    ],
+    "global": ["Lat", "Long"],
 }
 
 SUBSTITUTE_COLUMNS = {
-    "US": {
-        "Province_State":"Province/State",
-        "Country_Region":"Country/Region"
-    },
-    "global": {}
+    "US": {"Province_State": "Province/State", "Country_Region": "Country/Region"},
+    "global": {},
 }
 
 GITHUB_PREFIX = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
@@ -59,16 +68,18 @@ def import_johns_hopkins(rds: RegionDataset, prefix=None):
     ds = []
     for n in ["Recovered", "Confirmed", "Deaths"]:
         d = pd.read_csv(
-            f"{prefix}/time_series_covid19_{n.lower()}_global.csv", dtype="U",
-            usecols=lambda x: x not in DROP_COLUMNS["global"]
+            f"{prefix}/time_series_covid19_{n.lower()}_global.csv",
+            dtype="U",
+            usecols=lambda x: x not in DROP_COLUMNS["global"],
         )
         d = d.apply(pd.to_numeric, downcast="float", errors="ignore")
 
         for r in SUBDIVIDED_DATASETS:
             try:
                 rd = pd.read_csv(
-                    f"{prefix}/time_series_covid19_{n.lower()}_{r}.csv", dtype="U",
-                    usecols=lambda x: x not in DROP_COLUMNS[r]
+                    f"{prefix}/time_series_covid19_{n.lower()}_{r}.csv",
+                    dtype="U",
+                    usecols=lambda x: x not in DROP_COLUMNS[r],
                 )
             except (HTTPError, FileNotFoundError) as e:
                 log.info(f"Category '{n}' not found for regional record {r}")
@@ -78,13 +89,11 @@ def import_johns_hopkins(rds: RegionDataset, prefix=None):
 
             rd = rd.apply(pd.to_numeric, downcast="float", errors="ignore")
 
-            rd = (rd
-                .groupby(by=["Country/Region","Province/State"])
-                .agg("sum"))
+            rd = rd.groupby(by=["Country/Region", "Province/State"]).agg("sum")
             rd.reset_index(inplace=True)
-            d = pd.concat([d,rd])
-            #n.b. this yields unexpected results if the date columns in the global record do not match those in the US record
-        
+            d = pd.concat([d, rd])
+            # n.b. this yields unexpected results if the date columns in the global record do not match those in the US record
+
         d.reset_index(inplace=True)
 
         codes = np.full(len(d), "", dtype="U64")
@@ -124,13 +133,11 @@ def import_johns_hopkins(rds: RegionDataset, prefix=None):
 
         d.index = pd.Index(codes, name="Code")
 
-        for col in ["Country/Region", "Province/State","index"]:
+        for col in ["Country/Region", "Province/State", "index"]:
             del d[col]
         d.columns = pd.DatetimeIndex(pd.to_datetime(d.columns, utc=True), name="Date")
 
-        ds.append(
-            d.loc[d.index != ""].stack().to_frame(n)
-        )
+        ds.append(d.loc[d.index != ""].stack().to_frame(n))
 
     if skipped:
         log.info(f"Skipped {len(skipped)} records: {skipped!r}")
