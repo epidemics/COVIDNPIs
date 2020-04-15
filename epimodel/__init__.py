0d0f413adffc88f2db73701693ff042f3e62d67e
Tomas Gavenciak <gavento@gmail.com>
2020-04-15 03:35:13 +0200

    Update population algorithms and tests

--- epimodel/__init__.py
+++ epimodel/__init__.py
@@ -1,3 +1,3 @@
 from .regions import Region, RegionDataset, Level
 from .utils import read_csv, read_csv_names
-from . import imports, exports, gleam
+from . import imports, exports, gleam, algorithms
