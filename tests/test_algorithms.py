e80c47715b6d37f3fb40b242c0e30ca98a3a16e5
Tomas Gavenciak <gavento@gmail.com>
2020-04-15 11:13:37 +0200

    Test redistribution and add test

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-
+import pandas as pd
 from epimodel import algorithms
 
 
@@ -20,3 +20,15 @@ def test_estimate_pop(regions):
     algorithms.estimate_missing_populations(regions, root="W-EU")
     assert regions["CZ"].Population == pytest.approx(10000.0, rel=0.1)
     assert regions["SK"].Population == pytest.approx(10000.0, rel=0.1)
+
+
+def test_distribute_down(regions_gleam):
+    s = pd.Series([1e6, 9e5], index=["W", "W-NA"])
+    algorithms.distribute_down_with_population(s, regions_gleam)
+    s = s.sort_index()
+    assert s["W"] == 1e6
+    assert s["W-AS"] == pytest.approx(65000, rel=0.05)
+    assert s["W-EU"] == pytest.approx(10500, rel=0.05)
+    assert s["CZ"] == pytest.approx(150, rel=0.05)
+    assert s["G-PRG"] == pytest.approx(80, rel=0.1)
+    assert s["W-NA"] == 9e5
