beb6ead8e35b43fe33e83c304d08444aa597c053
Jon Wolverton <wolverton.jr@gmail.com>
2020-05-13 12:39:23 -0700

    get pytest fixtures working with unittest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,45 @@ import epimodel
 
 @pytest.fixture
 def datadir(request):
-    return Path(request.module.__file__).parent / "data"
+    return _datadir(request)
 
 
 @pytest.fixture
 def regions(datadir):
-    return epimodel.RegionDataset.load(datadir / "regions.csv")
+    return _regions(datadir)
 
 
 @pytest.fixture
 def regions_gleam(datadir):
+    return _regions_gleam(datadir)
+
+
+# unittest
+
+
+@pytest.fixture(scope="class")
+def ut_datadir(request):
+    request.cls.datadir = _datadir(request)
+
+
+@pytest.fixture(scope="class")
+def ut_rds(request, ut_datadir):
+    cls = request.cls
+    cls.rds = _regions_gleam(cls.datadir)
+
+
+# shared logic
+
+
+def _datadir(request):
+    return Path(request.module.__file__).parent / "data"
+
+
+def _regions(datadir):
+    return epimodel.RegionDataset.load(datadir / "regions.csv")
+
+
+def _regions_gleam(datadir):
     return epimodel.RegionDataset.load(
         datadir / "regions.csv", datadir / "regions-gleam.csv"
     )
--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -4,19 +4,15 @@ from . import PandasTestCase
 
 import pandas as pd
 
-import epimodel
-from epimodel.utils import utc_date
+from epimodel import Region, RegionDataset
 import epimodel.gleam.scenario as sc
 
 
+@pytest.mark.usefixtures("ut_datadir", "ut_rds")
 class TestConfigParser(PandasTestCase):
     def test_output_format(self):
-        parser = sc.ConfigParser(
-            rds=epimodel.RegionDataset.load(
-                "../data/regions.csv", "../data/regions-gleam.csv"
-            )
-        )
-        df = parser.get_config_from_csv("data/scenario_config.csv")  # self.config_csv)
+        parser = sc.ConfigParser(rds=self.rds)
+        df = parser.get_config_from_csv(self.datadir / "scenario_config.csv")
 
         self.assert_array_equal(
             df.columns, sc.ConfigParser.FIELDS, "output columns do not match"
@@ -30,7 +26,7 @@ class TestConfigParser(PandasTestCase):
 
         # Column types
         self.assert_dtype(df["Region"], "O")
-        self.assertIsInstance(df["Region"].dropna().iloc[0], epimodel.Region)
+        self.assertIsInstance(df["Region"].dropna().iloc[0], Region)
         self.assert_dtype(df["Value"], "float")
         self.assert_dtype(df["Start date"], "datetime64[ns]")
         self.assert_dtype(df["End date"], "datetime64[ns]")
