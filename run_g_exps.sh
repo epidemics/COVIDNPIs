13d866a6ce566aa1a34eaee49e8f040b9cf685ae
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-05-25 23:51:35 +0100

    bugfix

--- run_g_exps.sh
+++ run_g_exps.sh
@@ -3,5 +3,5 @@
 for g in 0.01 0.02 0.03 0.05 0.1 0.15 0.2 0.3 0.4
 do
      echo "running with noise $g"
-     python run_g_exp.py --g $g --s 2000 --c 4 --rg &
+     python run_g_exp.py --g $g --s 2000 --c 4&
 done
