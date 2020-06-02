jsalvatier/epimodel

Add hospital capacity

epimodel/exports/epidemics_org.py
@@ -13,6 +13,7 @@ import numpy as np
 from tqdm import tqdm
 
 from ..regions import Region
+import math as m
 
 log = logging.getLogger(__name__)
 
@@ -51,6 +52,7 @@ class WebExport:
         timezones: Optional[pd.DataFrame],
         un_age_dist: Optional[pd.DataFrame],
         traces_v3: Optional[pd.DataFrame],
+        hospital_capacity: Optional[pd.DataFrame],
     ):
         er = WebExportRegion(
             region,
@@ -62,6 +64,7 @@ class WebExport:
             timezones,
             un_age_dist,
             traces_v3,
+            hospital_capacity,
         )
         self.export_regions[region.Code] = er
         return er
@@ -99,12 +102,13 @@ class WebExportRegion:
         timezones: Optional[pd.DataFrame],
         un_age_dist: Optional[pd.DataFrame],
         traces_v3: Optional[pd.DataFrame],
+        hospital_capacity: Optional[pd.DataFrame],
     ):
         assert isinstance(region, Region)
         self.region = region
         # Any per-region data. Large ones should go to data_ext.
         self.data = self.extract_smallish_data(
-            rates, hopkins, foretold, timezones, un_age_dist, traces_v3
+            rates, hopkins, foretold, timezones, un_age_dist, traces_v3, hospital_capacity
         )
         # Extended data to be written in a separate per-region file
         self.data_ext = self.extract_models_data(models, simulations_spec)
@@ -119,6 +123,7 @@ class WebExportRegion:
         timezones: Optional[pd.DataFrame],
         un_age_dist: Optional[pd.DataFrame],
         traces_v3: Optional[pd.DataFrame],
+        hospital_capacity: Optional[pd.DataFrame],
     ) -> Dict[str, Dict[str, Any]]:
         d = {}
 
@@ -146,6 +151,14 @@ class WebExportRegion:
 
         if un_age_dist is not None:
             d["AgeDist"] = un_age_dist.to_dict()
+
+        if hospital_capacity is not None:
+            capacity = {}
+            for key, value in hospital_capacity.to_dict().items():
+                if not isinstance(value, float) or not np.isnan(value):
+                    capacity[key] = value
+            
+            d["Capacity"] = capacity
 
         return d
 
@@ -367,6 +380,8 @@ def process_export(args) -> None:
 
     traces_v3_df: pd.DataFrame = pd.read_csv(traces_v3, index_col="CodeISO3")
 
+    hospital_capacity_df = pd.read_csv(get_extra_path(args, "hospital_capacity"), index_col="Code")
+
     hopkins_df: pd.DataFrame = pd.read_csv(
         hopkins, index_col=["Code", "Date"], parse_dates=["Date"]
     ).pipe(aggregate_countries, args.config["state_to_country"])
@@ -392,6 +407,7 @@ def process_export(args) -> None:
             get_df_list(timezone_df, code),
             get_df_else_none(un_age_dist_df, m49),
             get_df_else_none(traces_v3_df, iso3),
+            get_df_else_none(hospital_capacity_df, code),
         )
 
     ex.write(args.config["output_dir"])

epidemics/COVIDNPIs

epimodel/exports/epidemics_org.py
@@ -57,6 +57,7 @@ class WebExport:
         timezones: Optional[pd.DataFrame],
         un_age_dist: Optional[pd.DataFrame],
         r_estimates: Optional[pd.DataFrame],
+        hospital_capacity: Optional[pd.DataFrame],
     ):
         export_region = WebExportRegion(
             region,
@@ -71,6 +72,7 @@ class WebExport:
             timezones,
             un_age_dist,
             r_estimates,
+            hospital_capacity,
         )
         self.export_regions[region.Code] = export_region
         return export_region
@@ -142,6 +144,7 @@ class WebExportRegion:
         timezones: Optional[pd.DataFrame],
         un_age_dist: Optional[pd.DataFrame],
         r_estimates: Optional[pd.DataFrame],
+        hospital_capacity: Optional[pd.DataFrame],
     ):
         log.debug(f"Prepare WebExport: {region.Code}, {region.Name}")
 
@@ -153,7 +156,7 @@ class WebExportRegion:
 
         # Any per-region data. Large ones should go to data_ext.
         self.data = self.extract_smallish_data(
-            rates, hopkins, foretold, timezones, un_age_dist, r_estimates
+            rates, hopkins, foretold, timezones, un_age_dist, r_estimates, hospital_capacity
         )
         # Extended data to be written in a separate per-region file
         self.data_ext = self.extract_external_data(models, simulation_specs)
@@ -168,6 +171,7 @@ class WebExportRegion:
         timezones: pd.DataFrame,
         un_age_dist: Optional[pd.DataFrame],
         r_estimates: Optional[pd.DataFrame],
+        hospital_capacity: Optional[pd.DataFrame],
     ) -> Dict[str, Dict[str, Any]]:
         data = {}
 
@@ -206,6 +210,14 @@ class WebExportRegion:
                 "Date": [x.isoformat() for x in r_estimates.index],
                 **r_estimates[["MeanR", "StdR"]].to_dict(orient="list"),
             }
+
+        if hospital_capacity is not None:
+            capacity = {}
+            for key, value in hospital_capacity.to_dict().items():
+                if not isinstance(value, float) or not np.isnan(value):
+                    capacity[key] = value
+
+            data["Capacity"] = capacity
 
         return data
 
@@ -523,6 +535,8 @@ def process_export(
         columns=["Type", "Region Name", "Parent Code M49"]
     )
 
+    hospital_capacity_df = pd.read_csv(get_extra_path(args, "hospital_capacity"), index_col="Code")
+
     foretold_df: pd.DataFrame = pd.read_csv(
         foretold,
         index_col=["Code", "Date"],
@@ -571,5 +585,6 @@ def process_export(
             get_df_list(timezone_df, code),
             get_df_else_none(un_age_dist_df, m49),
             get_df_else_none(r_estimates_df, code),
+            get_df_else_none(hospital_capacity_df, code),
         )
     return ex