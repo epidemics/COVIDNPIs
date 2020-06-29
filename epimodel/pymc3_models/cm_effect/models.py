8996cc5e99cfb7653f03c74f08c714f9a5c19ae2
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-06-29 22:38:58 +0100

    add plotters dirty

--- epimodel/pymc3_models/cm_effect/models.py
+++ epimodel/pymc3_models/cm_effect/models.py
@@ -2382,7 +2382,7 @@ class CMCombined_Final_V3(BaseCMModel):
         self.DelayProbDeaths = self.DelayProbDeaths.reshape((1, self.DelayProbDeaths.size))
 
         self.CMDelayCut = 30
-        self.DailyGrowthNoise = 0.3
+        self.DailyGrowthNoise = 0.7
 
         self.ObservedDaysIndx = np.arange(self.CMDelayCut, len(self.d.Ds))
         self.OR_indxs = np.arange(len(self.d.Rs))
