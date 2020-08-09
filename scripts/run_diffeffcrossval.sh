6345191085e28f3e258dba49507e7ad7007b881f
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-08-09 16:00:54 +0100

    bugfix

--- scripts/run_diffeffcrossval.sh
+++ scripts/run_diffeffcrossval.sh
@@ -7,9 +7,11 @@ for g in 0.15 0.175 0.2 0.225 0.25
 do
   for a in 0.05 0.075 0.1 0.125 0.15
   do
-     echo "Running Growth Noise $g region noise $a"
-     python scripts/run_diffeff_crossval.py --g $g --a $a &
-     sleep 30
+    if [ $a!="0.15" ] && [ $g!="0.25" ];then
+        echo "Running Growth Noise $g region noise $a"
+        python scripts/run_diffeff_crossval.py --g $g --a $a &
+        sleep 30
+    fi
    done
 done
 
