625df2cfcdb7935e4fcca045caffb88505838d09
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-08-26 16:20:46 +0100

    new grid

---
+++ scripts/run_epi_grid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+
+for gi in 0 1 2
+do
+  for cd in 0 1 2
+  do
+    for dd in 0 1
+    do
+        python scripts/run_epi_grid.py --cd $cd --dd $d --gi $gi &
+        sleep 30
+    done
+   done
+done
