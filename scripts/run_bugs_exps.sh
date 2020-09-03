99936557afdd8be61006c82638f146fd5548ac1d
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-09-03 21:48:33 +0100

    new params

--- scripts/run_bugs_exps.sh
+++ scripts/run_bugs_exps.sh
@@ -3,7 +3,7 @@ export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
-for i in {0..13}
+for i in {0..15}
 do
    echo "Running Experiment $i"
    python scripts/run_bugs_exp.py --exp $i &
