6733f70cb6550ceecc6975b32972296e4e950138
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-09-04 19:49:33 +0100

    bugfix

--- scripts/run_bootstrapped_exp.py
+++ scripts/run_bootstrapped_exp.py
@@ -8,7 +8,6 @@ os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 ### Initial imports
 import logging
-import copy
 import numpy as np
 import pymc3 as pm
 import seaborn as sns
@@ -23,12 +22,10 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from epimodel.preprocessing.data_preprocessor import preprocess_data
-from epimodel import EpidemiologicalParameters, DefaultModel
+from epimodel import EpidemiologicalParameters
 from epimodel.pymc3_models.models import DefaultModelFixedDispersion
 
 import argparse
-import pickle
-import time
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--base_seed', dest='seed', type=int)
@@ -55,17 +52,12 @@ if __name__ == '__main__':
                               reporting_delay=DelayProbCases,
                               fatality_delay=DelayProbDeaths, cm_prior=args.prior, cm_prior_scale=prior_scale)
 
-        time_start = time.time()
         with model.model:
             model.trace = pm.sample(1500, tune=500, cores=4, chains=4, max_treedepth=14, target_accept=0.95)
-        time_end = time.time()
-
-        model.trace.time_elapsed = time_end - time_start
 
         out_dir = f'bootstrapped_exps_{args.prior}'
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
-
         np.savetxt(f'{out_dir}/{seed}.txt', model.trace.CMReduction)
 
         seed += args.parallel_runs
