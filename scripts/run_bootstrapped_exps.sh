a9636a7ec9421c2590e025884a9b6770cd869fbd
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-09-05 00:49:03 +0100

    small alt

--- scripts/run_bootstrapped_exps.sh
+++ scripts/run_bootstrapped_exps.sh
@@ -4,10 +4,12 @@ export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
 PRIOR_TYPE=$1
+N_SMOOTH=$2
+ALPHA=$3
 
 for i in {0..23}
 do
-   python scripts/run_bootstrapped_exp.py --parallel_runs 24 --base_seed $i --prior $PRIOR_TYPE --n_runs 4 &
+   python scripts/run_bootstrapped_exp.py --parallel_runs 24 --base_seed $i --prior $PRIOR_TYPE --n_runs 4 --n_smooth $N_SMOOTH --alpha $ALPHA &
    sleep 30
 done
 
