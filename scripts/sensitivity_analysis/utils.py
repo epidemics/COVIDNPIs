eaf490cc4d8c893d948a75d15e4a3628c51df0dc
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2020-09-07 20:24:07 +0100

    more sensitivity tests

--- scripts/sensitivity_analysis/utils.py
+++ scripts/sensitivity_analysis/utils.py
@@ -6,6 +6,20 @@ import numpy as np
 def get_model_class_from_str(model_type_str):
     if model_type_str == 'default':
         return epm.DefaultModel
+    elif model_type_str == 'additive':
+        return epm.AdditiveModel
+    elif model_type_str == 'discrete_renewal_fixed_gi':
+        return epm.DiscreteRenewalFixedGIModel
+    elif model_type_str == 'discrete_renewal':
+        return epm.DiscreteRenewalModel
+    elif model_type_str == 'noisy_r':
+        return epm.NoisyRModel
+    elif model_type_str == 'different_effects':
+        return epm.DifferentEffectsModel
+    elif model_type_str == 'cases_only':
+        return epm.CasesOnlyModel
+    elif model_type_str == 'deaths_only':
+        return epm.DeathsOnlyModel
 
 
 def add_argparse_arguments(argparse):
