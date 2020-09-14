6df6809b24e602c17ccd74ea39e5c0fedf04fbf0
Tomas Witzany <tomas.witzany@blindspot.ai>
2020-09-14 16:28:04 +0200

    include the parent directory for autodoc of epimodel module

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,9 @@
 # import sphinx
 # sys.path.insert(0, os.path.abspath('.'))
 # sys.path.insert(0, os.path.abspath('..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
